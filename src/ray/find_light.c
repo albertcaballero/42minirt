a56be6a277fd7c6d2c160ffc66b5fd393e4b4895
Albert caballero <albertcaballerocoll@gmail.com>
2024-03-21 15:58:25 +0100

    shadows work

--- src/ray/find_light.c
+++ src/ray/find_light.c
@@ -6,7 +6,7 @@
 /*   By: alcaball <alcaball@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 15:46:48 by alcaball          #+#    #+#             */
-/*   Updated: 2024/03/14 13:04:08 by alcaball         ###   ########.fr       */
+/*   Updated: 2024/03/21 15:46:37 by alcaball         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,28 +19,23 @@ double	get_next_ligth(t_scene *scene, t_point point, t_hit *rec)
 	double	final;
 	t_hit	light_rec;
 	double	magnitude;
-	t_ray	inv;
 
 	// while (scene->light)
 	// {
 		light.origin = scene->light->pos;
 		light.dir = substract_vec(&light.origin, &point);
+		magnitude = length_vec(&light.dir);
 		light.dir = normalize_vec(&light.dir);
 
-		inv.origin = point;
-		inv.dir = substract_vec(&scene->light->pos, &inv.origin);
-		magnitude = length_vec(&inv.dir);
+		light.dir = substract_vec(&point, &light.origin);
+		light.dir = normalize_vec(&light.dir);
 		light_rec = nearest_hit(&light, scene);
-		if (light_rec.t > 0.0 && light_rec.t + 1e-6 < magnitude) //si se cumple es sombra y se encuentra or delate de la luz
-		{
-			
-			// printf("mag %f\n", magnitude);
+		light.dir = substract_vec(&light.origin, &point);
+		light.dir = normalize_vec(&light.dir);
+		if (light_rec.t > 0.0 && light_rec.t + 1e-6 < magnitude)
 			dotprod = -1.0;
-		}
 		else
 		{
-
-			// printf("else %f\n", light_rec.t);
 			dotprod = dot_scalar_product(&rec->normal, &light.dir);
 		}
 		if (dotprod < 0.0)
@@ -50,3 +45,8 @@ double	get_next_ligth(t_scene *scene, t_point point, t_hit *rec)
 	// }
 	return (final);
 }
+
+// t_color	add_ambient_light(t_color obj, t_ambient *ambient)
+// {
+	
+// }
