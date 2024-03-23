d60a09f9c511b10e386445d377b3435ac06d282f
Albert caballero <albertcaballerocoll@gmail.com>
2024-03-23 17:48:17 +0100

    trying to add ambient light

--- src/ray/cast.c
+++ src/ray/cast.c
@@ -6,24 +6,28 @@
 /*   By: alcaball <alcaball@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 10:42:07 by alcaball          #+#    #+#             */
-/*   Updated: 2024/03/23 15:26:20 by alcaball         ###   ########.fr       */
+/*   Updated: 2024/03/23 16:49:11 by alcaball         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minirt.h>
 
+t_color	illuminate(t_scene *scene, t_point point, t_hit *rec);
+
 t_color	ray_color(t_ray *ray, t_scene *scene)
 {
 	t_hit	rec;
-	double	light;
+	// double	light;
 	t_color	px_color;
 
 	rec = nearest_hit(ray, scene);
 	if (rec.t > 0)
 	{
-		px_color = mix_colors(rec.obj->col, scene->ambient.color, scene->ambient.ratio);
-		light = get_next_ligth(scene, ray_at(ray, rec.t), &rec);
-		return (mix_colors(scene->light->color, px_color, light));
+		// light = get_next_ligth(scene, ray_at(ray, rec.t), &rec);
+		// return (mix_colors(scene->light->color, px_color, light));
+		px_color = illuminate(scene, ray_at(ray, rec.t), &rec);
+		return (illuminate(scene, ray_at(ray, rec.t), &rec));
+		return (mix_colors(px_color, rec.obj->col, 1.0));
 	}
 	return (mix_colors(scene->ambient.color, new_color(100, 100, 100), 0.4));
 }
--- src/ray/find_light.c
+++ src/ray/find_light.c
@@ -6,7 +6,7 @@
 /*   By: alcaball <alcaball@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 15:46:48 by alcaball          #+#    #+#             */
-/*   Updated: 2024/03/23 15:58:43 by alcaball         ###   ########.fr       */
+/*   Updated: 2024/03/23 17:39:25 by alcaball         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,45 @@ double	get_next_ligth(t_scene *scene, t_point point, t_hit *rec)
 	// }
 	return (final);
 }
+
+t_color	illuminate(t_scene *scene, t_point point, t_hit *rec)
+{
+	t_ray	light;
+	double	dotprod;
+	t_color	final;
+	t_hit	light_rec;
+	double	magnitude;
+
+	final = rec->obj->col;
+	// while (scene->light)
+	// {
+		light.origin = scene->light->pos;
+		light.dir = substract_vec(&light.origin, &point);
+		magnitude = length_vec(&light.dir);
+
+		light.dir = substract_vec(&point, &light.origin);
+		light.dir = normalize_vec(&light.dir);
+		light_rec = nearest_hit(&light, scene);
+		light.dir = substract_vec(&light.origin, &point);
+		light.dir = normalize_vec(&light.dir);
+		if (light_rec.t > 0.0 && light_rec.t + 1e-6 < magnitude)
+			dotprod = -1.0;
+		else
+		{
+			dotprod = dot_scalar_product(&rec->normal, &light.dir);
+			if (dotprod > 0.0)
+			{
+				final = mix_colors(final, scene->light->color, dotprod*scene->light->ratio);
+			}
+		}
+		// scene->light = scene->light->next;
+	// }
+	final = mix_colors(scene->ambient.color, final, scene->ambient.ratio);
+	return (final);
+}
+
+
+//https://www.gamedev.net/blogs/entry/2260865-shadows-and-point-lights/ tiene muuy buena pinta
 
 /* shade(ray, point, normal, lights) 
 {
